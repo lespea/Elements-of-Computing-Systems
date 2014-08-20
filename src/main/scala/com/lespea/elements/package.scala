4315b6800c78156ffc9d3c2945d27e9c72444d79
Adam Lesperance <lespea@gmail.com>
2014-08-20 01:15:22 -0500

    Add io types

--- src/main/scala/com/lespea/elements/package.scala
+++ src/main/scala/com/lespea/elements/package.scala
@@ -1,5 +1,12 @@
 package com.lespea
 
 package object elements {
-
+  type IO1 = Boolean
+  type IO2 = (Boolean, Boolean)
+  type IO3 = (Boolean, Boolean, Boolean)
+  type IO4 = (Boolean, Boolean, Boolean, Boolean)
+  type IO5 = (Boolean, Boolean, Boolean, Boolean, Boolean)
+  type IO6 = (Boolean, Boolean, Boolean, Boolean, Boolean, Boolean)
+  type IO7 = (Boolean, Boolean, Boolean, Boolean, Boolean, Boolean, Boolean)
+  type IO8 = (Boolean, Boolean, Boolean, Boolean, Boolean, Boolean, Boolean, Boolean)
 }
