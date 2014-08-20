609fe62137cf29d695274dab8e6ef539f731919f
Adam Lesperance <lespea@gmail.com>
2014-08-20 01:15:46 -0500

    Define base transformers

--- src/main/scala/com/lespea/elements/basic/transformers.scala
+++ src/main/scala/com/lespea/elements/basic/transformers.scala
@@ -1,8 +1,13 @@
 package com.lespea.elements.basic
 
-/**
- * Created by Adam on 2014-08-17.
- */
-class transformers {
+import com.lespea.elements._
 
+sealed trait Transformer[A, B] {
+  def compute(in: A): B
 }
+
+abstract class Transform1To1 extends Transformer[IO1, IO1]
+
+abstract class Transform2To1 extends Transformer[IO2, IO1]
+
+abstract class Transorm2To2 extends Transformer[IO2, IO2]
