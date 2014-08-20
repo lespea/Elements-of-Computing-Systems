8e1824ecdc695ee5c1d29ae78076d9cbe23edda7
Adam Lesperance <lespea@gmail.com>
2014-08-20 01:16:21 -0500

    Test NAND gate

--- src/test/scala/testing/basic/BasicTests.scala
+++ src/test/scala/testing/basic/BasicTests.scala
@@ -1,5 +1,14 @@
 package testing.basic
 
-class BasicTests {
+import com.lespea.elements.basic.Nand
+import org.scalatest.WordSpec
 
+final class BasicTests extends WordSpec with LogicTests {
+  "A NAND gate" should {
+    behave like validLogicTable(Nand, Seq(
+      ((false, false), true),
+      ((true, false), true),
+      ((false, true), true),
+      ((true, true), false)))
+  }
 }
--- src/test/scala/testing/basic/LogicTests.scala
+++ src/test/scala/testing/basic/LogicTests.scala
@@ -1,5 +1,12 @@
 package testing.basic
 
-class LogicTests {
+import com.lespea.elements.basic.Transformer
+import org.scalatest.{ Matchers, Inspectors, WordSpec }
 
+trait LogicTests extends Inspectors with Matchers { this: WordSpec ⇒
+  final def validLogicTable[A, B, T <: Transformer[A, B]](transformer: T, tests: Seq[(A, B)]): Unit = {
+    "conform to its logic tests" in {
+      forAll(tests) { case (in, out) ⇒ transformer.compute(in) shouldBe out }
+    }
+  }
 }
