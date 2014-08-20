f506da3b10154bc78c1af19e542d1816022628b9
Adam Lesperance <lespea@gmail.com>
2014-08-20 01:15:50 -0500

    Define NAND gate

--- src/main/scala/com/lespea/elements/basic/Nand.scala
+++ src/main/scala/com/lespea/elements/basic/Nand.scala
@@ -3,5 +3,5 @@ package com.lespea.elements.basic
 import com.lespea.elements.{ IO1, IO2 }
 
 case object Nand extends Transform2To1 {
-  @inline def compute(in: IO2): IO1 = !(in._1 & in._2)
+  @inline final def compute(in: IO2): IO1 = !(in._1 & in._2)
 }
